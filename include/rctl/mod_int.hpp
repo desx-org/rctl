9396a8f958a24c75cbdbe7f5c90f85b61d612140
DESX <joelchenette@gmail.com>
2021-11-05 09:37:00 +0000

    mod int test working

---
+++ include/rctl/mod_int.hpp
@@ -0,0 +1,118 @@
+#pragma once
+
+#include <limits>
+#include <type_traits>
+#include <cstddef>
+
+template<typename T>
+constexpr T max_multiple_inv(const T v)
+{
+   auto ret = (std::numeric_limits<T>::max() - v + 1)%v;
+ 
+   return ret==0?v:ret;
+}
+
+template<typename T>
+constexpr bool is_power_of_2(const T val) 
+{
+   if(val == 0) return false;
+   else if(val == 1) return true;
+   else if(val & 1) return false; 
+   else return is_power_of_2(val >> 1);
+}
+
+template<typename T>
+constexpr T max_multiple(const T v)
+{
+   if (is_power_of_2(v)) 
+      return 0;
+   else
+   return std::numeric_limits<T>::max() - max_multiple_inv(v) + 1;
+}
+
+template<typename INT_TYPE ,INT_TYPE mod_val>
+class mod_int
+{
+   public:
+
+   using this_t = mod_int<INT_TYPE, mod_val>;
+
+   mod_int(INT_TYPE val_in)
+   {
+      if(!mod_val)
+         val_ = val_in;
+      else
+         val_ = val_in%mod_val;
+   };
+
+   mod_int operator + (this_t incr_val) const
+   {
+      if (!mod_val)
+      {
+         return this->val_ + incr_val.val_;
+      }
+      else
+      {
+         INT_TYPE new_val = (val_ + incr_val.val_);
+
+         if (!is_power_of_2(mod_val)) 
+         {
+            if(new_val < val_){ new_val += max_multiple_inv(mod_val);}
+         }
+
+         return new_val%mod_val;
+      }
+   }
+
+   mod_int operator ++ (int)
+   {
+      auto tmp = *this;
+      operator++();
+      return tmp;
+   }
+
+   mod_int operator -- (int)
+   {
+      auto tmp = *this;
+      operator--();
+      return tmp;
+   }
+
+   INT_TYPE val() const {return val_;}
+
+   mod_int operator += (this_t incr_val){return (*this = *this + incr_val);}
+   mod_int operator -= (this_t incr_val){ return operator+=(-incr_val);}
+
+   mod_int operator ++ (){return operator+=(1); }
+   mod_int operator -- (){return operator-=(1); }
+
+   mod_int operator - (){return mod_val - this->val_;}
+   mod_int operator - (this_t incr_val) const {return operator +(-incr_val);}
+
+   bool operator == (const INT_TYPE val_in) const { return (val_ == val_in); }
+   bool operator == (const mod_int val_in) const { return (val_ == val_in.val_);}
+   bool operator != (const mod_int val_in) const { return !(val_ == val_in.val_);}
+   
+   bool operator > (const mod_int val_in) const { return (val_ > val_in.val_);}
+
+   
+   private:
+   INT_TYPE val_;
+};
+
+template<typename T,size_t S>
+class mod_index:public mod_int<T,max_multiple<T>(S)>
+{
+   public:
+
+   using P = mod_int<T,max_multiple<T>(S)>;
+
+   mod_index(T v):P(v){}
+   mod_index(P v):P(v){}
+   using P::val;
+
+   T index()
+   {
+      return val()%S; 
+   }
+};
---
+++ tests/tests.cpp
@@ -0,0 +1,157 @@
+#include "rctl/mod_int.hpp"
+#include "fmt/format.h"
+#include "catch.hpp"
+
+#include <experimental/array>
+#include <vector>
+
+TEST_CASE("mod int test","")
+{
+   SECTION("is power of 2 test")
+   {   
+
+      REQUIRE(is_power_of_2<uint8_t>(1 ) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(2 ) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(4 ) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(8 ) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(16) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(32) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(64) == true); 
+      REQUIRE(is_power_of_2<uint8_t>(128) == true); 
+
+      REQUIRE(is_power_of_2<uint8_t>(  0) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(  3) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(  5) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(  6) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(  7) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(  9) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 10) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 11) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 12) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 13) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 14) == false); 
+      REQUIRE(is_power_of_2<uint8_t>( 33) == false); 
+      REQUIRE(is_power_of_2<uint8_t>(255) == false); 
+
+   }
+   SECTION("max multiple test")
+   {   
+      //REQUIRE(max_multiple<uint8_t>(  0) ==   0);//floating point error signal
+      REQUIRE(max_multiple<uint8_t>(  1) ==   0);
+      REQUIRE(max_multiple<uint8_t>(  2) ==   0);
+      REQUIRE(max_multiple<uint8_t>(  3) == 255);
+      REQUIRE(max_multiple<uint8_t>(  4) ==   0);
+      REQUIRE(max_multiple<uint8_t>(  5) == 255);
+      REQUIRE(max_multiple<uint8_t>(  6) == 252);
+      REQUIRE(max_multiple<uint8_t>(  7) == 252);
+      REQUIRE(max_multiple<uint8_t>(  8) ==   0);
+      REQUIRE(max_multiple<uint8_t>(  9) == 252);
+      REQUIRE(max_multiple<uint8_t>( 10) == 250);
+      REQUIRE(max_multiple<uint8_t>(100) == 200);
+      REQUIRE(max_multiple<uint8_t>(200) == 200);
+      REQUIRE(max_multiple<uint8_t>(250) == 250);
+      REQUIRE(max_multiple<uint8_t>(255) == 255);
+   }
+   SECTION("test mod int")
+   {   
+      mod_int<uint8_t, 10> v = 4;
+      auto b = ++v;
+      REQUIRE(b == 5); 
+      auto c = v++;
+      REQUIRE(c == 5); 
+
+      REQUIRE(v == 6); 
+      v+=1;
+      REQUIRE(v == 7); 
+      v+=2;
+      REQUIRE(v == 9); 
+      v+=1;
+      REQUIRE(v == 0); 
+      v+=10;
+      REQUIRE(v == 0); 
+      v=9;
+      REQUIRE(v == 9); 
+      v+=250;
+      REQUIRE(v == 9); 
+      v+=255;
+      REQUIRE(v == 4); 
+      auto y =v + 1;
+      REQUIRE(v == 4); 
+      REQUIRE(y == 5); 
+      v-=1;
+      REQUIRE(v.val() == 3); 
+      v-=3;
+      REQUIRE(v.val() == 0); 
+      v-=2;
+      REQUIRE(v.val() == 8); 
+      v-=10;
+      REQUIRE(v.val() == 8); 
+      v-=20;
+      REQUIRE(v.val() == 8); 
+      v-=250;
+      REQUIRE(v.val() == 8); 
+      mod_int<uint8_t, 0> q = 4;
+      REQUIRE(q.val() == 4); 
+      q +=251;
+      REQUIRE(q.val() == 255);
+      q +=1;
+      REQUIRE(q.val() == 0); 
+      q =1; 
+      q += 255;
+      REQUIRE(q.val() == 0); 
+
+      mod_int<uint8_t, 10> r = 4;
+      r += 255;
+      REQUIRE(r.val() == 9);
+      REQUIRE(r.val() == 9); 
+      r = 9;
+      r += 255;
+      REQUIRE(r.val() == 4);
+      {  
+         mod_int<uint8_t, 10> v = 0;
+         v-=2;
+         REQUIRE(v.val() == 8);
+      }
+      mod_int<uint32_t, 10> ss = 1;
+
+      REQUIRE(ss.val() == 1);
+      ss = -ss;
+      REQUIRE(ss.val() == 9);
+      ss = 10;
+      REQUIRE(ss.val() == 0);
+
+   }
+   SECTION("mod index test")
+   {
+      mod_index<uint8_t,5>  f1 = (uint8_t)22;
+      REQUIRE(f1.val() == 22);
+      REQUIRE(f1.index() == 2);
+      f1++; 
+      REQUIRE(f1.val() == 23);
+      REQUIRE(f1.index() == 3);
+      ++f1; 
+      REQUIRE(f1.val() == 24);
+      REQUIRE(f1.index() == 4);
+      ++f1; 
+      REQUIRE(f1.val() == 25);
+      REQUIRE(f1.index() == 0);
+      f1+=10; 
+      REQUIRE(f1.val() == 35);
+      REQUIRE(f1.index() == 0);
+      f1+=100; 
+      REQUIRE(f1.val() == 135);
+      REQUIRE(f1.index() == 0);
+      f1+=1; 
+      REQUIRE(f1.val() == 136);
+      REQUIRE(f1.index() == 1);
+      f1+=110; 
+      REQUIRE(f1.val() == 246);
+      REQUIRE(f1.index() == 1);
+      f1+=10; 
+      REQUIRE(f1.val() ==   1);
+      REQUIRE(f1.index() == 1);
+      f1-=10; 
+      REQUIRE(f1.val() == 246);
+      REQUIRE(f1.index() == 1);
+   }
+}
